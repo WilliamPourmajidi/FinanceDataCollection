c6a90e5070bc8ec3c777418578a4ba7326d2238f
William Pourmajidi <William.Pourmajidi@gmail.com>
2022-02-07 15:23:42 -0500

    Simple Code added

--- main.py
+++ main.py
@@ -3,13 +3,5 @@ from pandas_datareader import data as pdr
 
 msft = yf.Ticker("MSFT")
 
-# get stock info
-print(msft.info)
-
 data = yf.download("MSFT AAPL IBM", start="2021-01-01", end="2021-12-30")
-
 print(data)
-
-
-
-
